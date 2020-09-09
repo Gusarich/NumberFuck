88c43b3d47ff6d4e800fd5780a07e2a1ccf0d7aa
Daniel <tntplaypro@gmail.com>
2020-09-09 23:56:20 +0300

    something else

--- js/functions.js
+++ js/functions.js
@@ -37,10 +37,32 @@ function increaseFormulaValue () {
     if (score.gte(upgrades[0].cost)) {
         score = score.div(upgrades[0].cost);
         upgrades[0].cost = upgrades[0].cost.times(2);
+        upgrades[0].done = upgrades[0].done.plus(1);
         formula.value = formula.value.plus(1);
 
         document.getElementById('score').innerText = normalize(score);
         document.getElementById('upgradeCost1').innerText = normalize(upgrades[0].cost);
         document.getElementById('formula').innerText = 's(t) ' + '+ ' + normalize(formula.value);
+    }
+}
+
+function upgradeFormulaType () {
+    if (score.gte(upgrades[1].cost)) {
+        score = score.div(upgrades[1].cost);
+        upgrades[1].done += 1;
+        formula.type += 1;
+
+        if (upgrades[1].done == 1) {
+            upgrades[1].cost = upgrades[1].cost.pow(100);
+            document.getElementById('formula').innerText = 's(t) * 2';
+        }
+        else if (upgrades[1].done == 2) {
+            upgrades[1].cost = upgrades[1].cost.pow('eee1');
+            document.getElementById('formula').innerText = 's(t) ^ 2';
+        }
+
+        formula.value = new ExpantaNum('2');
+        document.getElementById('score').innerText = normalize(score);
+        document.getElementById('upgradeCost2').innerText = normalize(upgrades[1].cost);
     }
 }
--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@ var score = new ExpantaNum('1');
 var time = new ExpantaNum('0');
 var formula = {
     type: 1, // 1 +, 2 *, 3 ^, 4 x^()
-    value: new ExpantaNum('2')
+    value: new ExpantaNum('1')
 };
 var upgrades = [
     {
-        cost: new ExpantaNum('10')
+        cost: new ExpantaNum('10'),
+        done: new ExpantaNum('0')
+    },
+    {
+        cost: new ExpantaNum('1000'),
+        done: 0
     }
 ];
 
 document.getElementById('score').innerText = normalize(score);
 
-setInterval(tick, 500);
+setInterval(tick, 50);
